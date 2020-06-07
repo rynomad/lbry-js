aabdd55af7f5cbbaf3c85b7576a43f2cecdaf504
fbslo <admin@fbslo.net>
2020-06-07 19:39:57 +0200

    Added account_create API call

--- build/LbryDaemonClient.js
+++ build/LbryDaemonClient.js
@@ -243,6 +243,9 @@ class RPCClient {
   claim_search(params) {
     return this.request("claim_search", params);
   }
+  account_create(params) {
+    return this.request("account_create", params);
+  }
 }
 
 module.exports = RPCClient;
